828e3767875c1cbd00fb346e33321530f7b35972
Fahad Ali Shaikh <fshaikh@digite.com>
2023-09-13 15:54:11 +0530

    updated model

--- app.py
+++ app.py
@@ -5,13 +5,16 @@ import uuid6
 app = Robyn(__file__)
 agent = Agent.load("./models/20230913-154420-devout-envelope.tar.gz")
 
+
 @app.get("/")
 async def h(request):
     return "Hello, world!"
+
 
 @app.post("/chat")
 async def chat(request):
     print("chat")
     return await agent.handle_text("Hi", sender_id=uuid6.uuid7().hex)
+
 
 app.start(port=8080)
